adaa30858b7b8267e6352f6262061743725e5963
Alan <allan.tecnologia.ufms@gmail.com>
2023-05-18 20:55:15 -0400

    Novo teste em sala

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { darkTheme } from "./Theme";
+import {
+  AppBar,
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  TextField,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  useTheme,
+} from "@mui/material";
+
+function App() {
+  const theme = useTheme();
+  return (
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <AppBar position='static'>
+          <Toolbar
+            sx={{
+              paddingTop: theme.spacing(2),
+              paddingBottom: theme.spacing(2),
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              "@media all": {
+                minHeight: 200,
+              },
+            }}
+          >
+            <Typography variant='h5' component='h1'>
+              todo
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <Container>
+            <Grid container spacing={theme.spacing(0.5)}>
+              <Grid item xl={10} sm={12}>
+                <TextField name='task' fullWidth />
+              </Grid>
+              <Grid item xl={2} sm={12}>
+                    <Button variant="contained" fullWidth>Criar</Button>
+              </Grid>
+            </Grid>
+          </Container>
+        </main>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
